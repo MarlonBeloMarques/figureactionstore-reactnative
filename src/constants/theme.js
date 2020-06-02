d66b493d025bf0da49cc419e7967a814a09ad772
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-06-01 21:32:57 -0400

    config constants

---
+++ src/constants/theme.js
@@ -0,0 +1,49 @@
+const colors = {
+  primary: '#010101',
+  secondary: '#2E2F33',
+  tertiary: '#BF4A45',
+  white: '#FFFFFF',
+};
+
+const sizes = {
+  // global sizes
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+
+  // font sizes
+  h1: 26,
+  h2: 20,
+  h3: 18,
+  title: 18,
+  header: 16,
+  body: 14,
+  caption: 12,
+};
+
+const fonts = {
+  h1: {
+    fontSize: sizes.h1,
+  },
+  h2: {
+    fontSize: sizes.h2,
+  },
+  h3: {
+    fontSize: sizes.h3,
+  },
+  header: {
+    fontSize: sizes.header,
+  },
+  title: {
+    fontSize: sizes.title,
+  },
+  body: {
+    fontSize: sizes.body,
+  },
+  caption: {
+    fontSize: sizes.caption,
+  },
+};
+
+export { colors, sizes, fonts };
